8b780b86116865fbe3dbc3cd85b8027429631a64
DaveN8 <100126795+DaveN8@users.noreply.github.com>
2023-03-17 00:31:57 +0700

    handling final

--- 0706012110042-Dave Nathaniel Y-AFL 1/Toko.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Toko.swift
-//  0706012110042-Dave Nathaniel Y-AFL 1
-//
-//  Created by MacBook Pro on 03/03/23.
-//
-
-import Foundation
--- 0706012110042-Dave Nathaniel Y-AFL 1/main.swift
+++ 0706012110042-Dave Nathaniel Y-AFL 1/main.swift
@@ -9,14 +9,13 @@ import Foundation
 
 var userInput: String = ""
 var userInput2: Int = 0
-//var nama_food = ["Tahu Isi", "Nasi Kuning", "Nasi Campur", "Air Mineral"]
 let nama_toko: [String] = ["Tuku-Tuku", "GoTri", "Madam Lie", "Kopte", "Raburi"]
 let menuhuy1: [(String, Int)] = [("Tahu Isi", 20000), ("Nasi Kuning", 16000), ("Nasi Campur", 14000), ("Air Mineral", 12000)]
 let menuhuy2: [(String, Int)] = [("Nasi Bakar Rendang", 28000), ("Nasi Bakar Kare", 28000), ("Nasi Bakar Tuna", 28000), ("Nasi Bakar Cabe Hijau", 28000)]
 let menuhuy3: [(String, Int)] = [("Martabak Telur", 20000), ("Martabak Mesir", 25000), ("Martabak Bangka", 30000), ("Martabak Spesial", 35000)]
 let menuhuy4: [(String, Int)] = [("Kopi Tarik Kopte", 13000), ("Kopi Tarik Cincau", 14000), ("Kopi Teh Tarik", 15000), ("Kopi Coklat Tarik", 16000)]
 let menuhuy5: [(String, Int)] = [("Kare", 30000), ("Ramen", 33000), ("Sushi", 31000), ("Teh Pucuk", 8000)]
-var cart: [(namaToko: String, namaFood: String, jumlahFood: Int)] = []
+var cart: [(String, String, Int)] = []
 var total: Int = 0
 
 firstscreen()
@@ -25,7 +24,7 @@ func firstscreen(){
     print("""
         Welcome to UC Cafetaria👩‍🍳🧑‍🍳
         Please choose cafetaria:
-
+        
         """)
     
     var nomor: Int = 0
@@ -42,6 +41,7 @@ func firstscreen(){
         """, terminator: " ")
     
     userInput = readLine()!.lowercased()
+    print()
     
     switch userInput {
     case "q" :
@@ -67,20 +67,20 @@ func orderscreen(inputan: Int, toko: String){
     if toko == "Tuku-Tuku"{
         print("\(menuhuy1[inputan-1].0) @ \(menuhuy1[inputan-1].1)")
         print("How many \(menuhuy1[inputan-1].0) you want to buy?", terminator: " ")
-            userInput = readLine()!
+        userInput = readLine()!
         
         if (Int(userInput) ?? 0 > 0){
             let jumlah: Int = Int(userInput) ?? 0
             for (index, check) in cart.enumerated() {
-                    if check.1 == menuhuy1[inputan-1].0{
-                        let temp: Int = check.jumlahFood + jumlah
-                        cart.remove(at: index)
-                        cart.append((toko, menuhuy1[inputan-1].0, temp))
-                        print("ThankYou for Ordering")
-                        
-                        total += (menuhuy1[inputan-1].1 * (Int(userInput) ?? 0))
-                        menu(toko: toko)
-                    }
+                if check.1 == menuhuy1[inputan-1].0{
+                    let temp: Int = check.2 + jumlah
+                    cart.remove(at: index)
+                    cart.append((toko, menuhuy1[inputan-1].0, temp))
+                    print("ThankYou for Ordering")
+                    
+                    total += (menuhuy1[inputan-1].1 * (Int(userInput) ?? 0))
+                    menu(toko: toko)
+                }
             }
             cart.append((toko, menuhuy1[inputan-1].0, jumlah))
             
@@ -96,115 +96,123 @@ func orderscreen(inputan: Int, toko: String){
         }
         
     }else if toko == "GoTri"{
-            print("\(menuhuy2[inputan-1].0) @ \(menuhuy2[inputan-1].1)")
-            print("How many \(menuhuy2[inputan-1].0) you want to buy?", terminator: " ")
-                userInput = readLine()!
-        
-            if (Int(userInput) ?? 0 > 0){
-                let jumlah: Int = Int(userInput) ?? 0
-                for (index, check) in cart.enumerated() {
-                        if check.1 == menuhuy2[inputan-1].0{
-                            let temp: Int = check.jumlahFood + jumlah
-                            cart.remove(at: index)
-                            cart.append((toko, menuhuy2[inputan-1].0, temp))
-                            print("ThankYou for Ordering")
-                            
-                            total += (menuhuy2[inputan-1].1 * (Int(userInput) ?? 0))
-                            menu(toko: toko)
-                        }
-                }
-                cart.append((toko, menuhuy2[inputan-1].0, jumlah))
-                
-                print("ThankYou for Ordering")
-                
-                total += (menuhuy2[inputan-1].1 * (Int(userInput) ?? 0))
-                firstscreen()
-            }else{
-                menu(toko: toko)
-            }
-        
-    }else if toko == "Madam Lie"{
-            print("\(menuhuy3[inputan-1].0) @ \(menuhuy3[inputan-1].1)")
-            print("How many \(menuhuy3[inputan-1].0) you want to buy?", terminator: " ")
-                userInput = readLine()!
-        
-            if (Int(userInput) ?? 0 > 0){
-                let jumlah: Int = Int(userInput) ?? 0
-                for (index, check) in cart.enumerated() {
-                        if check.1 == menuhuy3[inputan-1].0{
-                            let temp: Int = check.jumlahFood + jumlah
-                            cart.remove(at: index)
-                            cart.append((toko, menuhuy3[inputan-1].0, temp))
-                            print("ThankYou for Ordering")
-                            
-                            total += (menuhuy3[inputan-1].1 * (Int(userInput) ?? 0))
-                            menu(toko: toko)
-                        }
-                }
-                cart.append((toko, menuhuy3[inputan-1].0, jumlah))
-                
-                print("ThankYou for Ordering")
-                
-                total += (menuhuy3[inputan-1].1 * (Int(userInput) ?? 0))
-                firstscreen()
-            }else{
-                menu(toko: toko)
-            }
-        
-    }else if toko == "Kopte"{
-        print("\(menuhuy4[inputan-1].0) @ \(menuhuy4[inputan-1].1)")
-        print("How many \(menuhuy4[inputan-1].0) you want to buy?", terminator: " ")
-            userInput = readLine()!
+        print("\(menuhuy2[inputan-1].0) @ \(menuhuy2[inputan-1].1)")
+        print("How many \(menuhuy2[inputan-1].0) you want to buy?", terminator: " ")
+        userInput = readLine()!
         
         if (Int(userInput) ?? 0 > 0){
             let jumlah: Int = Int(userInput) ?? 0
             for (index, check) in cart.enumerated() {
-                    if check.1 == menuhuy4[inputan-1].0{
-                        let temp: Int = check.jumlahFood + jumlah
-                        cart.remove(at: index)
-                        cart.append((toko, menuhuy4[inputan-1].0, temp))
-                        print("ThankYou for Ordering")
-                        
-                        total += (menuhuy4[inputan-1].1 * (Int(userInput) ?? 0))
-                        menu(toko: toko)
-                    }
+                if check.1 == menuhuy2[inputan-1].0{
+                    let temp: Int = check.2 + jumlah
+                    cart.remove(at: index)
+                    cart.append((toko, menuhuy2[inputan-1].0, temp))
+                    print("ThankYou for Ordering")
+                    
+                    total += (menuhuy2[inputan-1].1 * (Int(userInput) ?? 0))
+                    menu(toko: toko)
+                }
+            }
+            cart.append((toko, menuhuy2[inputan-1].0, jumlah))
+            
+            print("ThankYou for Ordering")
+            
+            total += (menuhuy2[inputan-1].1 * (Int(userInput) ?? 0))
+            print()
+            menu(toko: toko)
+        }else{
+            print("Please Enter The Right Amount!")
+            orderscreen(inputan: inputan, toko: toko)
+        }
+        
+    }else if toko == "Madam Lie"{
+        print("\(menuhuy3[inputan-1].0) @ \(menuhuy3[inputan-1].1)")
+        print("How many \(menuhuy3[inputan-1].0) you want to buy?", terminator: " ")
+        userInput = readLine()!
+        
+        if (Int(userInput) ?? 0 > 0){
+            let jumlah: Int = Int(userInput) ?? 0
+            for (index, check) in cart.enumerated() {
+                if check.1 == menuhuy3[inputan-1].0{
+                    let temp: Int = check.2 + jumlah
+                    cart.remove(at: index)
+                    cart.append((toko, menuhuy3[inputan-1].0, temp))
+                    print("ThankYou for Ordering")
+                    
+                    total += (menuhuy3[inputan-1].1 * (Int(userInput) ?? 0))
+                    menu(toko: toko)
+                }
+            }
+            cart.append((toko, menuhuy3[inputan-1].0, jumlah))
+            
+            print("ThankYou for Ordering")
+            
+            total += (menuhuy3[inputan-1].1 * (Int(userInput) ?? 0))
+            print()
+            menu(toko: toko)
+        }else{
+            print("Please Enter The Right Amount!")
+            orderscreen(inputan: inputan, toko: toko)
+        }
+        
+    }else if toko == "Kopte"{
+        print("\(menuhuy4[inputan-1].0) @ \(menuhuy4[inputan-1].1)")
+        print("How many \(menuhuy4[inputan-1].0) you want to buy?", terminator: " ")
+        userInput = readLine()!
+        
+        if (Int(userInput) ?? 0 > 0){
+            let jumlah: Int = Int(userInput) ?? 0
+            for (index, check) in cart.enumerated() {
+                if check.1 == menuhuy4[inputan-1].0{
+                    let temp: Int = check.2 + jumlah
+                    cart.remove(at: index)
+                    cart.append((toko, menuhuy4[inputan-1].0, temp))
+                    print("ThankYou for Ordering")
+                    
+                    total += (menuhuy4[inputan-1].1 * (Int(userInput) ?? 0))
+                    menu(toko: toko)
+                }
             }
             cart.append((toko, menuhuy4[inputan-1].0, jumlah))
             
             print("ThankYou for Ordering")
             
             total += (menuhuy4[inputan-1].1 * (Int(userInput) ?? 0))
-            firstscreen()
-        }else{
+            print()
             menu(toko: toko)
+        }else{
+            print("Please Enter The Right Amount!")
+            orderscreen(inputan: inputan, toko: toko)
         }
         
     }else if toko == "Raburi"{
         print("\(menuhuy5[inputan-1].0) @ \(menuhuy5[inputan-1].1)")
         print("How many \(menuhuy5[inputan-1].0) you want to buy?", terminator: " ")
-            userInput = readLine()!
+        userInput = readLine()!
         
         if (Int(userInput) ?? 0 > 0){
             let jumlah: Int = Int(userInput) ?? 0
             for (index, check) in cart.enumerated() {
-                    if check.1 == menuhuy5[inputan-1].0{
-                        let temp: Int = check.jumlahFood + jumlah
-                        cart.remove(at: index)
-                        cart.append((toko, menuhuy5[inputan-1].0, temp))
-                        print("ThankYou for Ordering")
-                        
-                        total += (menuhuy5[inputan-1].1 * (Int(userInput) ?? 0))
-                        menu(toko: toko)
-                    }
+                if check.1 == menuhuy5[inputan-1].0{
+                    let temp: Int = check.2 + jumlah
+                    cart.remove(at: index)
+                    cart.append((toko, menuhuy5[inputan-1].0, temp))
+                    print("ThankYou for Ordering")
+                    
+                    total += (menuhuy5[inputan-1].1 * (Int(userInput) ?? 0))
+                    menu(toko: toko)
+                }
             }
             cart.append((toko, menuhuy5[inputan-1].0, jumlah))
             
             print("ThankYou for Ordering")
             
             total += (menuhuy5[inputan-1].1 * (Int(userInput) ?? 0))
-            firstscreen()
-        }else{
+            print()
             menu(toko: toko)
+        }else{
+            print("Please Enter The Right Amount!")
+            orderscreen(inputan: inputan, toko: toko)
         }
     }
     
@@ -250,13 +258,13 @@ func menu(toko: String){
             print("[\(nomor)] \(towko.0)")
         }
     }
-
+    
     print("""
         -
         [B]ack to Main Menu
         Your Menu Choice?
         """, terminator: " ")
-            userInput = readLine()!.lowercased()
+    userInput = readLine()!.lowercased()
     
     switch userInput {
     case "b" :
@@ -274,16 +282,29 @@ func menu(toko: String){
     }
 }
 
+//func orderFrom(index: Int)->Bool{
+//    for shop in cart{
+//        if shop.0 == nama_toko[index]{
+//            return true
+//        }
+//    }
+//    return false
+//}
+
 func cartuhuy(){
     if cart.isEmpty {
         print("Kosong bruv")
         print()
     }else{
         for i in 0..<nama_toko.count{
-            print("Your Order From \(nama_toko[i]):")
-            for ihiy in cart{
-                if ihiy.0 == nama_toko[i]{
-                    print("- \(ihiy.1) X\(ihiy.2)")
+            if cart.contains(where: {$0.0 == nama_toko[i]}) {
+                print("""
+                      Your order from \(nama_toko[i]) :
+                      """)
+            }
+            for item in cart{
+                if item.0 == nama_toko[i]{
+                    print("- \(item.1) X\(item.2)")
                 }
             }
         }
@@ -314,45 +335,54 @@ func payout(){
           Your Total: \(total)
           Please Enter The Amount of your Money:
           """, terminator: " ")
-    let amount: Int? = Int(readLine()!)
+    userInput = readLine()!.lowercased()
     
-    if amount == nil {
+    
+    if userInput == "" {
         print("Please enter Your payment")
         payout()
-    }else if amount != nil {
-        if amount == 0{
-            print("The Amount cannot be zero")
-            payout()
-        }else if amount! < 0 {
-            print("Please enter the valid amount")
-            payout()
-//        }else if amount.{
-        }else if amount! > 0{
-            if amount! >= total{
-                print("""
-                Your total order: \(total)
-                You Pay: \(amount!)
-                your changes: \(amount!-total)
-                
-                Enjoy Your Meal
-                
-                """)
-            }else{
-                print("Your Money Kurang Woi")
+    }else{
+        let amount: Int? = Int(userInput)
+        if amount != nil {
+            if amount == 0{
+                print("The Amount cannot be zero")
                 print()
                 payout()
+            }else if amount! < 0 {
+                print("Please enter the valid amount")
+                print()
+                payout()
+                //        }else if amount.{
+            }else if amount! > 0{
+                if amount! >= total{
+                    print("""
+                    Your total order: \(total)
+                    You Pay: \(amount!)
+                    your changes: \(amount!-total)
+                    
+                    Enjoy Your Meal
+                    
+                    """)
+                }else{
+                    print("Your Money Kurang Woi")
+                    print()
+                    payout()
+                }
+                repeat{
+                    print("Press [return] to go back to main screen")
+                    userInput = readLine()!.lowercased()
+                }while userInput != "return"
+                cart.removeAll()
+                total = 0
+                print()
+                firstscreen()
             }
-            repeat{
-                print("Press [return] to go back to main screen")
-                userInput = readLine()!.lowercased()
-            }while userInput != "return"
-            cart.removeAll()
-            total = 0
+        }else{
+            print("Please enter the valid amount")
             print()
-            firstscreen()
+            payout()
         }
     }
 }
 
-// error handling untuk cart biar ga tampil semua
-// kurang error handling untuk inputan checkout harus int
+
